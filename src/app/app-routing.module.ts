737de3dfb9e069fc292bbc0629e12b7f78c5767f
John Evans <john.wes.evans@gmail.com>
2019-09-08 20:41:21 -0500

    routing and whatnot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from "../app/users/users.component";
+import { PostsComponent } from "../app/posts/posts.component";
+import { DetailsComponent } from "../app/details/details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent
+  },
+  {
+    path: "posts",
+    component: PostsComponent
+  },
+  {
+    path: "details/:id",
+    component: DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
